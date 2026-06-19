fc21275dfe48030e00e51e54ada691a52e451d8a
tebsss17 <137368444+tebsss17@users.noreply.github.com>
2026-06-20 04:23:50 +0800

    finished modal functions

--- resources/views/pages/transactions/⚡index.blade.php
+++ resources/views/pages/transactions/⚡index.blade.php
@@ -1,24 +1,108 @@
 <?php
 
 use Livewire\Component;
+use App\Models\Transaction;
 
 new class extends Component
 {
+    // VARIABLES
     public $showAddModal = false;
     public $showEditModal = false;
 
     public $category_id = '';
     public $amount = '';
-    public $type = '';
+    public $type = 'expense';
     public $description = '';
-    public $date = '';
+    public $transaction_date = '';
+
+    public $selectedTransaction = '';
 
 
+    // QUERY FUNCTIONS
+    public function mount()
+    {
+        $this->transaction_date = now()->format('Y-m-D');
+    }
 
+    public function transactions()
+    {
+        return Auth::user()
+            ->transaction()
+            ->with('category')
+            ->orderBy('transaction_date', 'desc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+
+    public function categories()
+    {
+        return Auth::user()->category()->orderBy('name')->get();
+    }
+
+    public function getStatsProperty()
+    {
+        $transactions = $this->transactions();
+
+        $income = $transactions->where('type', 'income')->sum('amount');
+        $expense = $transactions->where('type', 'expense')->sum('amount');
+        $balance = $income - $expense;
+
+        return[
+            'income' => $income,
+            'expense' => $expense,
+            'balance' => $balance,
+        ];
+    }
+
+
+    // MODAL FUNCTIONS
     public function addModal()
     {
+        $this->type = 'expense';
+        $this->transaction_date = now()->format('Y-m-d');
         $this->showAddModal = true;
     }
+
+    public function closeAddModal()
+    {
+        $this->reset([
+            'category_id',
+            'amount',
+            'type',
+            'description',
+        ]);
+        $this->resetErrorBag();
+        $this->showAddModal = false;
+    }
+
+    public function editModal($id)
+    {
+        $this->selectedTransaction = Auth::user()->transaction()->findOrFail($id);
+
+        $this->category_id = $this->selectedTransaction->category_id;
+        $this->amount = $this->selectedTransaction->amount;
+        $this->type = $this->selectedTransaction->type;
+        $this->description = $this->selectedTransaction->description;
+        $this->transaction_date = $this->selectedTransaction->transaction_date;
+
+        $this->showEditModal = true;
+    }
+
+    public function closeEditModal()
+    {
+        $this->reset([
+            'category_id',
+            'amount',
+            'type',
+            'description',
+        ]);
+        $this->resetErrorBag();
+        $this->showEditModal = false;
+    }
+
+
+    // CRUD FUNCTIONS
+
 };
 ?>
 
@@ -57,9 +141,7 @@ new class extends Component
 
     {{-- KPI Cards --}}
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <x-card>
 
-        </x-card>
     </div>
 
 </div>
